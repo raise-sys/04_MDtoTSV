cb57e7d66c131318cbf28ee00d616117952a94e5
raise-sys <raise.sys@gmail.com>
2021-01-04 03:09:18 +0900

    変換ロジックv0.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 function syori(){
   var eFrom = document.getElementById("txtFrom");
   var eTo = document.getElementById("txtTo");
-  eTo.textContent = eFrom.value;
+  var lineA = eFrom.value.split('\n');
+
+  var txtTo = "";
+  
+  var stime="";
+  var etime="";
+  var strNaiyo ="";
+  for(i=2; i<lineA.length; i++){
+    var line = String(lineA[i]);
+    var valA =  line.split('｜');
+    if (i===2){
+      stime = valA[1];
+      strNaiyo = valA[2];
+    }else{
+      etime = valA[1];
+      var strTime = stime + '\t' + etime;
+      txtTo = txtTo + "\n" + strTime + '\t' + strNaiyo;
+      
+      strNaiyo = valA[2];
+      stime = etime;
+    }
+  }
+  eTo.textContent = txtTo;
 
 }
+
